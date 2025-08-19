1ca1fb66d83b99d7adbac1535be0d49730b8b177
headlinetoday <headlinetoday056@gmail.com>
2025-08-19 14:59:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -364,5 +364,5 @@ def not_found(e):
 if __name__ == "__main__":
     # Use the port Koyeb provides (default 8080), fallback to 5000 locally
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
