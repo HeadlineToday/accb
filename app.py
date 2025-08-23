746ab6f684bf22642e90e04becdd0b36ea289623
headlinetoday <headlinetoday056@gmail.com>
2025-08-23 09:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,6 @@ def create_post():
         ext = os.path.splitext(secure_filename(image_file.filename))[1].lower()
         fname = f"{uuid.uuid4().hex}{ext}"
         file_bytes = image_file.read()
-        file_bytes = image_file.read()
         supabase.storage.from_(SUPABASE_BUCKET).upload(
             f"posts/{fname}", file_bytes, {"content-type": image_file.mimetype}
 )
